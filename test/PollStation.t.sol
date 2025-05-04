f44e9c413fdc9a66c74775df3a2e053494b0ce41
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-04 07:42:17 +0100

    testAddSucces

--- test/PollStation.t.sol
+++ test/PollStation.t.sol
@@ -11,5 +11,27 @@ contract PollStationTest is Test {
         pollStation = new PollStation();
     }
 
+    function testAddCandidate () public {
+        string[] memory Array = new string[](3);
+
+        Array[0] = "Alice";
+        Array[1] = "Bob";
+        Array[2] = "Sinclair";
+
+        pollStation.addCandidate(Array);
+
+        string[] memory candidate = pollStation.getCandidate();
+
+        assertEq(candidate[0], "Alice");
+        assertEq(candidate[1], "Bob");
+        assertEq(candidate[2], "Sinclair");
+        assertEq(pollStation.getVote("Alice"), 0);
+        assertEq(pollStation.getVote("Bob"), 0);
+        assertEq(pollStation.getVote("Sinclair"), 0);
+
+        
+        
+    }
+
     
 }
